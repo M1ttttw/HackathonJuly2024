6689e3a38ed451a0027a90cd8e57a225169c03a5
M1ttttw <matt_tay2003@outlook.com>
2024-07-28 23:33:51 -0700

    Scraper has begun

--- SkipScrapper.py
+++ SkipScrapper.py
@@ -1,3 +1,17 @@
-import browser
-import FoodClasses
+from browser import sd_init
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from FoodClasses import Restaurant
+import time
 
+test_addr = "4820 201 st"
+test_food = "Beef"
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+web = webdriver.Chrome(options=options)
+
+def sd_home_scrape():
+    sd_init("4820 201 st", web)
+
+    
