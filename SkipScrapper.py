80324fc257d2d8ab1bbeebde93c958a282559a6d
M1ttttw <matt_tay2003@outlook.com>
2024-07-31 22:19:50 -0700

    See Item is almost fixed

--- SkipScrapper.py
+++ SkipScrapper.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from typing import Any, Optional
+
 from browser import sd_init
 from browser import wait_and_grab, wait_for_elem
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -88,6 +90,7 @@ def sd_home_scrape(addr, food, limit=5):
         rest = Restaurant(rest_name, rest_addr, "SkipTheDishes", rest_rate, -1, rest_deliv_fee, -1,
                           rest_deliv_time)
 
+        addr_entered = False
         item_section_lst = mega_container.find_elements(By.XPATH, "*")
         for section in item_section_lst:
             # There are some sections that could be talking about place settings, so we skip over those
@@ -115,7 +118,7 @@ def sd_home_scrape(addr, food, limit=5):
                 item_info_bits = item.text.split("\n")
 
                 # It's possible for the item to be sold out, so check for it, and skip over it if needed
-                if item_info_bits[-1] == "SOLD OUT" or item_info_bits[-1] == "See Item":
+                if item_info_bits[-1] == "SOLD OUT":
                     print("Item sold out")
                     continue
 
@@ -129,17 +132,38 @@ def sd_home_scrape(addr, food, limit=5):
                     item_desc = item.find_element(By.XPATH, ".//div[contains(@class, "
                                                             "'styles__Description-sc-1xl58bi-7 wvRWw')]"
                                                             "/div").get_attribute("aria-label")
-                    item_price = float(item_info_bits[2][1:])
-                    # if item_info_bits[-1] == "See Item":
-                    #     # Open the item in view, and grab the price
-                    #     print("Found a see item")
-                    #     see_btn = item.find_element(By.XPATH, ".//div/div[1]")
-                    #
-                    #     see_btn.click()
-                    #     time.sleep(10)
-                    #
-                    # else:
-                    #     item_price = float(item_info_bits[2][1:])
+
+                    if item_info_bits[-1] == "See Item":
+                        # Open the item in view, and grab the price
+                        print("Found a see item")
+                        item.click()
+
+                        # Check if you ever entered your address before
+                        if not addr_entered:
+                            # Grab the text field for putting your address
+                            addr_fld = wait_and_grab(rest_driver, By.XPATH, "/html/body/div[2]/div/div[1]/div/header/div/div/div[1]/div[2]/div/div[2]/div[1]/div/div/div/div[3]/div[2]/div/div/div/div/div[1]/div[2]/form/input", 15)
+                            addr_fld.send_keys(addr)
+
+                            # Grab the first address that pops up
+                            addr_elem = wait_and_grab(rest_driver, By.XPATH, "/html/body/div[2]/div/div[1]/div/header/div/div/div[1]/div[2]/div/div[2]/div[1]/div/div/div/div[3]/div[2]/div/div/div/div[1]/div[2]/div/div/div[1]", 15)
+                            addr_elem.click()
+
+                            submit_btn = wait_and_grab(rest_driver, By.XPATH, "/html/body/div[2]/div/div[1]/div/header/div/div/div[1]/div[2]/div/div[2]/div[1]/div/div/div/div[3]/div[2]/div/div/div/div[4]/button", 15)
+                            submit_btn.click()
+
+                            addr_entered = True
+                            wait_for_elem(rest_driver, By.XPATH, '//*[@id="container"]/div/div/div/div[2]/div/div/div/div/div/div/div/div/div[1]/div[2]')
+                            item.click()
+
+                        # Grab the price
+                        close_btn = wait_and_grab(rest_driver, By.CSS_SELECTOR, ".MuiButtonBase-root.MuiIconButton-root.styles__StyledIconButton-sc-nhn5sv-0.cHIlwY", 20)
+                        item_price = float(rest_driver.find_element(By.CSS_SELECTOR, ".styles__Right-sc-gvk0sj-3.ixLLwq").text[1:])
+
+                        action = ActionChains(rest_driver)
+                        action.move_to_element(close_btn).click().perform()
+
+                    else:
+                        item_price = float(item_info_bits[2][1:])
 
                 print(item_name, item_desc, item_price)
 
